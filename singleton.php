67b3a5594c85db6a194798f49429a7042481664b
ing-sosa <ing.asosam@gmail.com>
2015-10-06 16:10:03 -0500

    Avoid clone()

--- singleton.php
+++ singleton.php
@@ -1,29 +1,52 @@
 <?php 
 /* 
 Sigleton Pattern 
-
-One instance of class
-values should remain intact
-
-Database use
 */
-
-class Database {
-	private function __construct(){
-		echo "Connection created";
-		}
-	private function __clone(){ }
+class Singleton
+{
+	private static $instance = null;
+	private $contador;
 	
-	public static function Connect(){
-		if (!isset(self::$connection)){
-			self:$connection = new Database;
-			}
-		return self:$connection;
+	private function __construct(){
+		echo __CLASS__." has been created.<br />";
+		$this->contador = 0;
 	}
+	
+	public static function getInstance()
+	{
+		if (self::$instance == null)
+		{
+			self::$instance = new Singleton();
+		}
+		return self::$instance;
+	}
+	
+	// prevent construction and cloning because of Singleton Pattern
+	public function __clone(){ 
+		trigger_error("You cannot clone an instance of ". get_class($this) ." class.", E_USER_ERROR );
+	}
+	
+	public function add(){
+		return ++$this->contador;
+	}
+	
+	public function sub(){
+		return --$this->contador;
+	}
+	
 }
+### Testing Singleton
 
-$db1 = Database::Connect();
-$db2 = Database::Connect();
+$obj1 = Singleton::getInstance();
+echo "obj1 (add): ".$obj1->add(). "<br />";
+echo "obj1 (add): ".$obj1->add(). "<br />";
+echo "obj1 (add): ".$obj1->add(). "<br />";
+$obj2 = Singleton::getInstance();
+echo "obj2 (sub): ".$obj2->sub(). "<br />";
+echo "obj2 (add): ".$obj2->add(). "<br />";
+$obj3 = clone($obj1);
+echo "obj3 (sub): ".$obj3->sub(). "<br />";
+echo "obj3 (add): ".$obj3->add(). "<br />";
 
 
 ?>
